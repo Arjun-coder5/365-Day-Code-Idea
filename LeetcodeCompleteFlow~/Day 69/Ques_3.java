9aa76e919d3240d61361106bc02d0ff2b416788a
Arjun-coder5 <arjunsorout526@gmail.com>
2026-03-16 18:53:55 +0530

    ok

---
+++ LeetcodeCompleteFlow~/Day 69/Ques_3.java
@@ -0,0 +1,33 @@
+
+public class Ques_3 {
+public static void main(String[] args) {
+  
+}
+    public int numIslands(char[][] grid) {
+          if (grid == null || grid.length == 0) return 0;
+        int numIslands = 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == '1') {
+                    numIslands++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return numIslands;
+    }
+     private void dfs(char[][] grid, int r, int c) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        if (r < 0 || c < 0 || r >= rows || c >= cols || grid[r][c] == '0') {
+            return;
+        }
+        grid[r][c] = '0';
+        dfs(grid, r - 1, c); 
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c - 1); 
+        dfs(grid, r, c + 1); 
+    }
+}
